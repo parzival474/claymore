a710e50d42c6d41bec37d4fa4952ffc9f6afc78b
parzival474 <62749863+parzival474@users.noreply.github.com>
2020-03-27 17:24:11 +0530

    Add files via upload

---
+++ eurethermine.bash
@@ -0,0 +1,1 @@
+./ethdcrminer64 -epool eu1.ethermine.org:4444 -ewal 0x165b3416c5f0087c14403eba84d2cc578e34b919 -epsw x -mode 1 -dbg -1 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -ttli 77 -tstop 89 
